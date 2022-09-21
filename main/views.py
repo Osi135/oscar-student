08caf2e8d632f907340b548c755fced405bd3257
Osi135 <oscar.horn.roostami@outlook.com>
2022-09-21 18:43:31 +0200

    Added test Html-site

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(response):
     return render(response, "main/home.html", {})
+
+def test(response):
+    return render(response, "main/test.html", {})
