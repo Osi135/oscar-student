8c4ce68ad9383d485e65c9407c58921978b33d89
Osi135 <oscar.horn.roostami@outlook.com>
2022-09-28 09:51:47 +0200

    Static files should now be possible

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,10 @@ import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = Path(__file__).resolve().parent.parent # Original
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # Test
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,11 +43,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'main.apps.MainConfig',
+    "main.apps.MainConfig",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -113,14 +117,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Original
+STATIC_ROOT = BASE_DIR / "staticfiles" # Alternative from whitenoise
+
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'), )
+STATICFILES_DIRS = [BASE_DIR / "static/",]
 
 django_heroku.settings(locals())
